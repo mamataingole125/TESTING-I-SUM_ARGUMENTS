c34816199b9586ca804550498f632fe2565a2e6a
mamataingole125 <mamataingole125@gmail.com>
2022-07-12 14:36:13 +0530

    jest testing for sum is added

---
+++ Jest_lect/index.js
@@ -0,0 +1,17 @@
+function sum(...args){
+
+  console.log(args)
+    if(args.length===0){
+  return "sum invoked without arguments"
+    }
+    else if(args.length===1){
+  return "sum function needs atleast 2 arguments"
+    }
+    
+  const sumValue=[...args].reduce((a,c)=>a+Number(c),0)  
+
+  return Number(sumValue.toFixed(1))
+
+}
+
+module.exports=sum;
